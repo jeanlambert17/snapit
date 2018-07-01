278f7b29636285fbf38d0b5832dc228e4704f63e
Jean Lambert <jeeaan1702@gmail.com>
2018-07-01 16:25:42 -0700

    fixing modules

--- app/src/scenes/index.js
+++ app/src/scenes/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from './Home/home';
 export { default as Login } from './Login/login';
-export { default as SingnUp } from './SignUp/signup';
+export { default as SignUp } from './SignUp/signUp';
