a36c19191f5bc315f14de665ecb8495ba6d7daa2
Jean Lambert <jeeaan1702@gmail.com>
2018-07-07 22:21:23 -0700

    changePassword endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -30,42 +30,31 @@ controllers.signUp = (req,res) => {
 }
 
 controllers.logIn = (req,res) => {
-    let { username, password } = req.body;
-    console.log(username);
-    console.log(password);
+    const { username, password } = req.body;
+    const send = ({status,body, ...rest}) => res.status(status).send({status,body, ...rest});
     User.findOne({ 'username': username }, async (err,user) => {
-        console.log(user)
-            if(err) 
-                res.status(500).send({
-                    status: 500,
-                    body: 'Try again' 
-                });
-            if(!user) 
-                res.status(401).send({ 
-                    status: 401,
-                    body: 'Username does not exist' 
-                });
+        if(err) send({ status: 500, body: 'Try again' });
+        if(!user) send({ status: 401, body: 'Username does not exist' });
+        try {
             let isMatch = await bcrypt.compare(password, user.password);
-            if (!isMatch) 
-                res.status(401).send({
-                    status: 401,
-                    body:'Wrong password',
-                });
-            
+            if (!isMatch) send({ status: 401, body: 'Invalid credentials' });
             let token = jwt.sign({ id: user._id }, configs.secret, { expiresIn: 86400 }); // 24 hours
             console.log('User login: ');
             console.log(user);
-            res.status(200).send({ 
-                status: 200, 
+            send({
+                status: 200,
                 body: {
                     username: user.username,
                     name: user.name,
                     email: user.email,
                 },
-                token: token 
+                token: token
             });
+        } catch(err) {
+            console.log(err);
+            send({ status: 500, body: 'Try again' });
         }
-    );
+    });
 
 }
 
@@ -77,28 +66,33 @@ controllers.changeField = (req,res) => {
     User.findOne({ _id: id }, async (err, user) => {
         if (err) send({ status: 500, body: 'Try again' });
         if (!user) send({ status: 500, body: 'Unable to reach user data' });
-        let isMatch = await bcrypt.compare(password, user.password);
-        if (isMatch) {
-            User.findOne({ [key]: value }, key, async (err, _user) => {
-                if (err) send({ status: 500, body: 'Try again later' });
-                if (_user) // 403 - El servidor ha podido ser contactado, y ha recibido una petición válida, pero ha denegado el acceso a la acción que se solicita
-                    send({ status: 403, body: 'Value already exist' });
-                else { // Update user data
-                    user.set({ [key]: value });
-                    user.save((err, updatedUser) => {
-                        if (err) send({ status: 400, body: 'Try again later' });
-                        if (updatedUser) send({
-                            status: 200,
-                            body: {
-                                username: updatedUser.username,
-                                name: updatedUser.name,
-                                email: updatedUser.email,
-                            }
+        try {
+            let isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                User.findOne({ [key]: value }, key, async (err, _user) => {
+                    if (err) send({ status: 500, body: 'Try again later' });
+                    if (_user) // 403 - El servidor ha podido ser contactado, y ha recibido una petición válida, pero ha denegado el acceso a la acción que se solicita
+                        send({ status: 403, body: 'Value already exist' });
+                    else { // Update user data
+                        user.set({ [key]: value });
+                        user.save((err, updatedUser) => {
+                            if (err) send({ status: 400, body: 'Try again later' });
+                            if (updatedUser) send({
+                                status: 200,
+                                body: {
+                                    username: updatedUser.username,
+                                    name: updatedUser.name,
+                                    email: updatedUser.email,
+                                }
+                            });
                         });
-                    });
-                }
-            });
-        } else send({ status: 401, body: 'Invalid credentials' });
+                    }
+                });
+            } else send({ status: 401, body: 'Invalid credentials' });
+        } catch(err) {
+            console.log('catch err: ' + err);
+            send({ status: 500, body: 'Try again' });
+        }
     });
 }
 
@@ -106,18 +100,23 @@ controllers.changePassword = (req,res) => {
     const { password, newPassword } = req.body;
     const id = req.userId;
     const send = ({ status, body }) => res.status(status).send({ status, body });
-    
+
     User.findOne({ _id: id }, async (err, user) => {
         if (err) send({ status: 500, body: 'Try again' });
         if (!user) send({ status: 500, body: 'Unable to reach user data' });
-        let isMatch = await bcrypt.compare(password, user.password);
-        if(isMatch) {
-            let hash = await bcrypt.hashSync(password, 10);
-            user.password = newPassword;
-            user.save((err,updatedUser) => {
-                if(err) send({ status: 500, body: 'Try again' });
-                send({ status: 200, body: 'Success' });
-            });
+        try {
+            let isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                let hash = await bcrypt.hashSync(newPassword, 10);
+                user.password = hash;
+                user.save((err,updatedUser) => {
+                    if(err) send({ status: 500, body: 'Try again' });
+                    send({ status: 200, body: 'Success' });
+                });
+            } else send({ status: 401, body: 'Invalid credentials' });
+        } catch(err) {
+            console.log('catch err: ' + err);
+            send({ status: 500, body:'Try again' });
         }
     });
 }
