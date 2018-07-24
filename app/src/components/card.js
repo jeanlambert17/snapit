9a033ce3eebcbd7fbfc05c604118f7a7328a46ce
fabioepb <djfabiokk@gmail.com>
2018-07-23 23:17:58 -0400

    Reworked Cards

--- app/src/components/card.js
+++ app/src/components/card.js
@@ -2,22 +2,57 @@ import React, { PureComponent } from 'react';
 import { 
   View, 
   Text, 
-  Image 
+  Image,
+  TouchableOpacity
 } from 'react-native'
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { Card, ListItem, Button, Icon, Avatar } from 'react-native-elements'
 import { API_URL } from '../helpers/configs';
 // import { connect } from 'react-redux';
 
 const _Card = ({onPress, ...props}) => (
     <Card
-      title={props.title}
-      image={{uri:`${API_URL}/${props.imageUrl}`}}
-      style={{width: 50, height: 50}}
-    >
-    <Button 
-      title="See comments"
-      onPress={onPress}
-    />
+      style={{width: 50, height: 50}}>
+      <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#dbdbdb'}}>
+        <Avatar
+          small
+          rounded
+          source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}} 
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+          containerStyle={{marginRight: 10}} />
+        <View style={{marginVertical: 2}}>
+          <Text style={{fontSize: 16, fontWeight: 'bold'}}>{props.title}</Text>
+          <Text style={{fontSize: 12, color: 'gray'}}>by {props.user}</Text>
+        </View>
+      </View>
+      <Image 
+        source={{uri:`${API_URL}/${props.imageUrl}`}}
+        style={{width: 300, height: 150, justifyContent:'center', marginTop: 5  }}/>
+      <View style={{marginVertical: 8}}>
+        <View style={{flexDirection:'row', marginVertical: 3}}>
+          <TouchableOpacity
+            /*onPress={}*/
+              style={{paddingRight:15}}>
+            <Icon name='thumbsup'
+              type='octicon' 
+              size={22} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            /*onPress={}*/>
+            <Icon name='thumbsdown'
+              type='octicon'/>
+          </TouchableOpacity>
+        </View>
+        <View style={{marginTop: 5}}>
+          <TouchableOpacity>
+            <Text style={{fontSize: 14, fontWeight: "bold"}}>{props.user}</Text>
+          </TouchableOpacity>
+          <Text style={{fontSize: 12, color:'#474747', marginBottom: 10, marginLeft: 10}}>{props.content}</Text>
+          <TouchableOpacity>
+            <Text style={{color:'gray'}}>See comments</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </Card>
 );
 
